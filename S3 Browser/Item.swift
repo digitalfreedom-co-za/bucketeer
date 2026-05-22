cddfce84b450b1da348fdb973e351cc17808e967
Marcel R. G. Berger <hello@marcelrgberger.com>
2026-05-22 15:10:05 +0200

    Initial Commit

---
+++ S3 Browser/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  S3 Browser
+//
+//  Created by Marcel Berger on 22.05.26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
